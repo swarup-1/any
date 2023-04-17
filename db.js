738d36dd4bb1a970552bfbe365fdd579376ea08f
swarup-1 <swarupkadoli9@gmail.com>
2023-04-17 18:12:31 +0530

    changed

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ const mongoose  = require("mongoose");
 
 require("dotenv").config();
 
-const connection = mongoose.connect(`mongodb+srv://kanban1:kanban1@cluster0.jbhxvfw.mongodb.net/kanban?retryWrites=true&w=majority`);
+const connection = mongoose.connect("mongodb+srv://kanban1:kanban1@cluster0.jbhxvfw.mongodb.net/kanban?retryWrites=true&w=majority");
 
 module.exports = {connection}
